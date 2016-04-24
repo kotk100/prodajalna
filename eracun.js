b7ab16af29660dd33a98fa75356b9bcd9f395f3a
Žiga Kern <kern.ziga@gmail.com>
2016-04-24 13:00:10 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -195,6 +195,8 @@ var vrniRacune = function(callback) {
   );
 }
 
+
+var sporociloPrijava = "";
 // Registracija novega uporabnika
 streznik.post('/prijava', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
@@ -208,14 +210,15 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Address, City, State, Country, PostalCode, \
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-      //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      stmt.run(polja['FirstName'], polja['LastName'], polja['Company'], polja['Address'], polja['City'], polja['State'], polja['Country'], polja['PostalCode'], polja['Phone'], polja['Fax'], polja['Email'], 3); 
+      stmt.finalize();
+      sporociloPrijava = "Stranka je bila uspešno registrirana.";
     } catch (err) {
       napaka2 = true;
+      sporociloPrijava = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
     }
   
-    odgovor.end();
+    odgovor.redirect('/prijava');
   });
 })
 
@@ -223,7 +226,7 @@ streznik.post('/prijava', function(zahteva, odgovor) {
 streznik.get('/prijava', function(zahteva, odgovor) {
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
-        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+        odgovor.render('prijava', {sporocilo: sporociloPrijava, seznamStrank: stranke, seznamRacunov: racuni});
       }) 
     });
 })
